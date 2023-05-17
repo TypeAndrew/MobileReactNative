1189c8d49ff50a771d41ddebb4c81951385fbde5
shulyar <andy.shular@gmail.com>
2023-05-18 00:44:48 +0300

    Add SvgXML

--- components/Screens/PostsScreen/PostsScreen.jsx
+++ components/Screens/PostsScreen/PostsScreen.jsx
@@ -1,50 +1,38 @@
 import {  View, Text, ImageBackground, StyleSheet } from "react-native";
 //import { TouchableOpacity } from "react-native-web";
+import  { SvgXml, SvgWithCss } from "react-native-svg";
+
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { Ionicons } from "@expo/vector-icons";
 
-export const PostsScreen1 = () =>{
+const xmlUser = `<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M20 21V19C20 16.7909 18.2091 15 16 15H8C5.79086 15 4 16.7909 4 19V21" stroke="#212121" stroke-opacity="0.8" stroke-linecap="round" stroke-linejoin="round"/>
+        <path fill-rule="evenodd" clip-rule="evenodd" d="M12 11C14.2091 11 16 9.20914 16 7C16 4.79086 14.2091 3 12 3C9.79086 3 8 4.79086 8 7C8 9.20914 9.79086 11 12 11Z" stroke="#212121" stroke-opacity="0.8" stroke-linecap="round" stroke-linejoin="round"/>
+      </svg>`;
 
-   return (
+const xmlRectangle = `<svg width="70" height="40" viewBox="0 0 70 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+      
+        <rect width="70" height="40" rx="20" fill="#FF6C00"/>
+        <text x="35" y="25" font-size="20" text-anchor="middle" dominant-baseline="middle" fill="#fff">+</text>
+        </svg>
+`;
+      
+const xmlGrid = `<svg width="40" height="40" viewBox="0 0 40 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <rect width="24" height="24" transform="translate(8 8)" fill="white"/>
+                <path fill-rule="evenodd" clip-rule="evenodd" d="M11 11H18V18H11V11Z" stroke="#212121" stroke-opacity="0.8" stroke-linecap="round" stroke-linejoin="round"/>
+                <path fill-rule="evenodd" clip-rule="evenodd" d="M22 11H29V18H22V11Z" stroke="#212121" stroke-opacity="0.8" stroke-linecap="round" stroke-linejoin="round"/>
+                <path fill-rule="evenodd" clip-rule="evenodd" d="M22 22H29V29H22V22Z" stroke="#212121" stroke-opacity="0.8" stroke-linecap="round" stroke-linejoin="round"/>
+                <path fill-rule="evenodd" clip-rule="evenodd" d="M11 22H18V29H11V22Z" stroke="#212121" stroke-opacity="0.8" stroke-linecap="round" stroke-linejoin="round"/>
+                </svg>`;      
 
-        <>
-         
-            <View style={styles.header}>
-                    <Text style={styles.headerText}>Publications</Text>        
-                </View>   
-            <View style={styles.formx}>
-                <View style={styles.AvatarRow}>
-                    <View style={styles.AvatarWrapper}>
-                        <ImageBackground
-                            style={styles.imageAvatar}
-                            > 
-                                
-                        </ImageBackground> 
-                        
-                    </View>
-                    <View style={styles.textCentral}><Text >user nane</Text><Text >email</Text></View>
-                    
-                </View>         
-            </View>
-            <View style={styles.bottomBar}>
-                <ImageBackground
-                style={styles.image}
-                source={require('../../../assets/icons/grid.svg')}/> 
-                <ImageBackground
-                style={[styles.image,styles.btnAdd]}
-                source={require("../../../assets/icons/Rectangle.svg")}><Text>+</Text></ImageBackground>   
-                <ImageBackground
-                style={styles.image}
-                source={require("../../../assets/icons/user.svg")}/>       
-            </View>
-        
-        </>
+ const xmlHomeLine = `<svg width="134" height="5" viewBox="0 0 134 5" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <rect width="134" height="5" rx="2.5" fill="#212121"/>
+                  </svg>`
+               
 
 
-    )
-}
 
-function grid() {
+function Grid() {
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
        <ImageBackground
@@ -56,7 +44,7 @@ function grid() {
 
 function Rectangle() {
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+    <View >
       <ImageBackground
                 style={[styles.image,styles.btnAdd]}
                 source={require("../../../assets/icons/Rectangle.svg")}><Text>+</Text></ImageBackground>   
@@ -65,12 +53,13 @@ function Rectangle() {
   );
 }
 
-function user() {
+function User() {
+
+
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <ImageBackground
-                style={styles.image}
-                source={require("../../../assets/icons/user.svg")}/>     
+      
+     <SvgXml xml={xmlUser} width="24" height="24" /> 
          
     </View>
   );
@@ -80,170 +69,50 @@ const Tabs = createBottomTabNavigator();
 
 export const PostsScreen = () => {
   return (
-    <Tabs.Navigator
-      screenOptions={({ route }) => ({
-        tabBarIcon: ({ focused, color, size }) => {
-          let iconName;
+    <View style={{ height: 700 }}>
+      <Tabs.Navigator 
+   
+        screenOptions={{ 
+    
+          tabBarStyle: {
+             height: 83, 
+            
+          },
+         
 
-          if (route.name === "Profile") {
-            iconName = focused
-              ? "ios-information-circle"
-              : "ios-information-circle-outline";
-          } else if (route.name === "Settings") {
-            iconName = focused ? "ios-list-box" : "ios-list";
-          }
-          return <Ionicons name={iconName} size={size} color={color} />;
-        },
-      })}
-      tabBarOptions={{
-        activeTintColor: "tomato",
-        inactiveTintColor: "gray",
+        
       }}
-    >
-        <Tabs.Screen name="grid" component={grid} 
+     
+      >
+        
+      <Tabs.Screen  style={{ height: 400 }} name="Grid" component={Grid} activeTintColor={"tomato"}
+        inactiveTintColor={"gray"}
           options={{
-          tabBarLabel: '',
-          tabBarIcon: ({ focused }) => (
-            <ImageBackground
-              source={focused ? require('../../../assets/icons/grid.svg') : require('../../../assets/icons/grid.svg')}
-              style={styles.image}
-            />
-          ),
+          tabBarLabel: 'Grid',
+          tabBarIcon: () => (
+              <SvgXml xml={xmlGrid} width="40" height="40" />
+            ),
         }}/>
-     <Tabs.Screen name="Rectangle" component={Rectangle} 
+     <Tabs.Screen name="Rectangle" component={Rectangle} activeTintColor={"tomato"}
+        inactiveTintColor={"gray"}
           options={{
-          tabBarLabel: '',
-          tabBarIcon: ({ focused }) => (
-            <ImageBackground
-              source={focused ? require('../../../assets/icons/Rectangle.svg') : require('../../../assets/icons/Rectangle.svg')}
-              style={[styles.image,styles.btnAdd]}
-            ><Text>+</Text></ImageBackground> 
-          ),
+          tabBarLabel: 'Rectangle',
+          tabBarIcon: () => (
+          <SvgXml xml={xmlRectangle} width="70" height="40"/>
+         ),
         }}/>
      
-    <Tabs.Screen name="user" component={user} 
+    <Tabs.Screen name="User" component={User} 
           options={{
-          tabBarLabel: '',
-          tabBarIcon: ({ focused }) => (
-            <ImageBackground
-              source={focused ? require('../../../assets/icons/user.svg') : require('../../../assets/icons/user.svg')}
-              style={styles.image}
-            />
-          ),
+          tabBarLabel: 'User',
+          tabBarIcon: () => (
+        <SvgXml xml={xmlUser} width="40" height="40" />
+         ),
         }} />
-    </Tabs.Navigator>
+      </Tabs.Navigator>
+      </View>
   );
 };
 
 const styles = StyleSheet.create({
-    container: {
-
-        flexDirection: 'column',
-        flex: 1,
-        backgroundColor: "#fff",
-      
-    },
-    bottomBar: {
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        flexDirection: 'row',
-        height: 89,
-        
-        width: 375,
-        elevation: 2,
-        shadowColor: '#000',
-        shadowOffset: { width: 0, height: 1 },
-        shadowOpacity: 0.3,
-        shadowRadius: 1,
-    },
-    formx: {
-    
-        width: 375,
-        height: 641,
-        color: 'black',
-        width: 375,
-        elevation: 2,
-        shadowColor: '#000',
-        shadowOffset: { width: 0, height: 1 },
-        shadowOpacity: 0.3,
-        shadowRadius: 1,
-    },
-    header: {
-        
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        width: 375,
-        height: 88,
-        fontStyle: 'normal',
-        fontWeight: 500,
-        fontSize: 17,
-        lineHeight: 22,
-        
-        elevation: 2,
-        shadowColor: '#000',
-        shadowOffset: { width: 0, height: 1 },
-        shadowOpacity: 0.3,
-        shadowRadius: 1,
-    },
-    headerText: {
-        
-        fontFamily: 'Roboto-Regular',
-        fontStyle: 'normal',
-        fontWeight: 500,
-        fontSize: 17,
-        lineHeight: 22,
-        
-
-    },
-    imageAvatar: {
-      
-   
-        width: 60,
-        height: 60,
-    },
-    AvatarWrapper: {
-         backgroundColor: '#F6F6F6',     
-        marginTop: 32,
-        marginLeft: 16,
-        color: 'black',
-        overflow: 'hidden',
-        resizeMode: 'cover',
-        borderRadius: 10,
-        width:  60,
-        height: 60,
-    },
-    AvatarRow: {
-        display: 'flex',
-        flexDirection: 'row',
-        alignContent: 'center',
-        margin: (0,0),
-    },
-    textCentral: {
-        marginTop: 32,
-        marginLeft: 8,
-        display: 'flex',
-        justifyContent: 'center',
-        alignContent: 'center',
-    },
-    image: {
-      
-   
-        marginLeft: 31,
-        width: 40,
-        height: 40,
-    },
-    btnAdd: {
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        width: 70,
-        color: 'white',
-       
-    },
-    text: {
-        margin: (0,0),
-       
-    },
 })
